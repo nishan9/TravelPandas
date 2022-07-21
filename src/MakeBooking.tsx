6b73a75c0381beb6ffdb0e95811eb09f2e86fc4d
Nishan Mahamalimage <nishan@Nishans-MacBook-Pro.local>
2022-07-21 19:43:57 +0800

    Booking functionality

--- src/MakeBooking.tsx
+++ src/MakeBooking.tsx
@@ -6,6 +6,12 @@ import Bookings from './Bookings';
 import { deepPurple, grey } from '@mui/material/colors';
 import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
 import CalendarKey from './components/CalendarKey';
+import { useSnackbar } from 'notistack';
+import { Player } from '@lottiefiles/react-lottie-player';
+import panda from './Images/json/panda.json'
+import format from 'date-fns/format';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from "react-router-dom";
 
 interface MakeBookingProps{
     date : string
@@ -17,9 +23,14 @@ interface MakeBookingProps{
 function MakeBooking(props : MakeBookingProps) {
 
     const [booking, setBooking] = useState<Booking>(); 
+    const { enqueueSnackbar } = useSnackbar();
+    const Auth0 = useAuth0();
+    const navigate = useNavigate();
+
 
     useEffect(() => {
         getData(); 
+
     }, [props.date])
     
     async function getData(){
@@ -35,8 +46,55 @@ function MakeBooking(props : MakeBookingProps) {
         }
         else 
         {
-            alert("Not Bookings Found For that date")
-            setBooking(undefined) 
+            enqueueSnackbar("Date is available", { variant: 'success' });
+            setBooking(undefined)
+        }
+    }
+
+    async function joinInbound(){
+
+        const token = await Auth0.getAccessTokenSilently();
+        const newtravller = {
+            "driver_id": props.driver,
+            "date" : props.date,
+        }
+
+        const res = await fetch(`https://localhost:5001/Bookings/Inbound`, {
+            method:"POST", 
+            body : JSON.stringify(newtravller), 
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization" : `Bearer ${token}`, 
+            }
+        })
+
+        if (res.ok){
+            enqueueSnackbar("Congragulations! Your booking is confirmed", { variant: 'success' });
+            //navigate("Dashboard");
+        }
+
+    }
+
+    async function joinOutbound(){
+
+        const token = await Auth0.getAccessTokenSilently();
+        const newtravller = {
+            "driver_id": props.driver,
+            "date" : props.date,
+        }
+
+        const res = await fetch(`https://localhost:5001/Bookings/Outbound`, {
+            method:"POST", 
+            body : JSON.stringify(newtravller), 
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization" : `Bearer ${token}`, 
+            }
+        })
+
+        if (res.ok){
+            enqueueSnackbar("Congragulations! Your booking is confirmed", { variant: 'success' });
+            //navigate("Dashboard");
         }
     }
 
@@ -45,7 +103,7 @@ function MakeBooking(props : MakeBookingProps) {
     <div className="driver-profile">
          {booking !== undefined ? 
          <>
-            <Typography variant={'h3'}>Reserve for {props.date} </Typography>
+            <Typography variant={'h4'}>Reserve for {format(new Date(props.date), 'EEEE do MMMM')} </Typography>
             <br/>
             <Typography>Driver Name: {props.driverName}</Typography>
             <Typography>Driver ID: {booking.driver_Id} </Typography>
@@ -73,7 +131,15 @@ function MakeBooking(props : MakeBookingProps) {
                             </Grid>
                         </>
                     )}
-                    {booking.inboundPassengers.length >= props.capacity ? <> <Button disabled={true} variant="contained"> Fully Booked</Button> </> : <> Book Now</> }
+                    {booking.inboundPassengers.length >= props.capacity 
+                    ? 
+                        <> <Button disabled={true} variant="contained"> Fully Booked</Button> </> 
+                    :
+                    <>
+                        <Button variant="contained" disabled={booking.inboundPassengers.map(x => x.auth0_Id).includes(Auth0.user?.sub!)} onClick={joinInbound}> Join Carpooling</Button> 
+                    </>  
+                    }
+
                 </Grid>
                 <Grid item xs={6}>
                 <Typography variant={'h5'} >Outbound Passengers</Typography> 
@@ -93,15 +159,61 @@ function MakeBooking(props : MakeBookingProps) {
                             </Grid>
                         </>
                     )}
-                    {booking.outboundPassengers.length >= props.capacity ? <> <Button variant="contained"> Fully Booked</Button> </> : <> <Button variant="contained"> Book Now</Button> </> }
+                    {booking.outboundPassengers.length >= props.capacity 
+                    ?
+                    <><Button variant="contained"> Fully Booked</Button> </> 
+                    :
+                    <Button variant="contained" disabled={booking.outboundPassengers.map(x => x.auth0_Id).includes(Auth0.user?.sub!)} onClick={joinOutbound}> Join Carpooling</Button> 
+                    }
                 </Grid>
             </Grid>
            
         </>
         :
-        <p>No Bookings Found</p>}
+       <>
+            {props.date != "" ? 
+            <>
+                <Typography variant={'h4'}>Reserve for {format(new Date(props.date), 'EEEE do MMMM')} </Typography>
+                <Grid container xs={12}>
+                    <Grid item xs={6}>
+                        <Typography>Inbound</Typography>
+                        <Button variant="contained" onClick={joinInbound}> Join Carpooling</Button>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography>Outbound</Typography>
+                        <Button variant="contained" onClick={joinOutbound}> Join Carpooling</Button>
+                    </Grid>
+                </Grid>
+                <Player
+                autoplay
+                loop
+                src={panda}
+                style={{ height: '200px' }}
+                >
+                </Player>
+            </>
+            :
+            <>
+             Please select a date heheheheh
+             <Player
+                autoplay
+                loop
+                src={panda}
+                style={{ height: '200px' }}
+                >
+                </Player>
+            </>
+            }
+
+       </>
+       
+       }
     </div>
   )
 }
 
 export default MakeBooking
+
+function enqueueSnackbar(arg0: string, arg1: { variant: string; }) {
+    throw new Error('Function not implemented.');
+}
